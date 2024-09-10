0972de1232e41b3ee74cdc289b08d6852780c05e
Arviaardhan <arviaardhan@gmail.com>
2024-09-10 14:03:09 +0700

    style : add carousel slider in homepage

--- lib/screens/homepage/home_screen.dart
+++ lib/screens/homepage/home_screen.dart
@@ -21,7 +21,7 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         surfaceTintColor: Colors.white,
         title: Text(
-          'Football Master',
+          'Balbalan',
           style: TextStyle(
             fontFamily: 'Oswald',
             fontWeight: FontWeight.bold,
@@ -35,12 +35,6 @@ class HomeScreen extends StatelessWidget {
               // Handle search action
             },
           ),
-          IconButton(
-            icon: Icon(Icons.person),
-            onPressed: () {
-              // Handle profile action
-            },
-          ),
         ],
       ),
       body: Padding(
@@ -48,34 +42,6 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            CarouselSlider(
-              options: CarouselOptions(
-                height: screenHeight * 0.3,
-                aspectRatio: 16/1,
-                viewportFraction: 1.0,
-                autoPlay: true,
-                autoPlayInterval: Duration(seconds: 3),
-                enlargeCenterPage: true,
-              ),
-              items: [
-                'assets/images/image-1.jpg',
-                'assets/images/laliga.jpg',
-                'assets/images/image-1.jpg',
-              ].map((imageUrl) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return ClipRRect(
-                      borderRadius: BorderRadius.circular(15),
-                      child: Image.asset(
-                        imageUrl,
-                        fit: BoxFit.cover,
-                      ),
-                    );
-                  },
-                );
-              }).toList(),
-            ),
-            SizedBox(height: 20,),
             Text(
               'List League',
               style: TextStyle(
@@ -84,15 +50,15 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             SizedBox(height: 10,),
-            Container(
-              height: screenHeight * 0.1,
-              child: Obx(() {
-                if (homeController.isLoading.value) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (homeController.infoLeagueModel.isEmpty) {
-                  return const Center(child: Text('No teams found'));
-                } else {
-                  return ListView.builder(
+            Obx(() {
+              if (homeController.isLoading.value) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (homeController.infoLeagueModel.isEmpty) {
+                return const Center(child: Text('No teams found'));
+              } else {
+                return Container(
+                  height: screenHeight * 0.1,
+                  child: ListView.builder(
                     scrollDirection: Axis.horizontal,
                     itemCount: homeController.infoLeagueModel.length,
                     itemBuilder: (context, index) {
@@ -118,22 +84,34 @@ class HomeScreen extends StatelessWidget {
                               default:
                                 imageAssetPath = 'assets/images/premier-bg.png';
                             }
+                            bool isSelected = homeController.selectedLeague.value == league.strLeague;
                             return InkWell(
                               onTap: () {
-                                print('strLeague : ${league.strLeague}');
+                                homeController.selectLeague(league.strLeague);
+                                print('Selected league: ${league.strLeague}');
                               },
                               child: Container(
-                                margin: EdgeInsets.only(right: screenWidth * 0.02),
+                                padding: EdgeInsets.all(10),
+                                margin: EdgeInsets.only(right: screenWidth * 0.04),
                                 decoration: BoxDecoration(
                                   shape: BoxShape.circle,
-                                  color: Colors.grey.shade300,
+                                  color: isSelected ? Color(0xFF381D53) : Colors.grey.shade100,  // Color change logic
                                 ),
                                 child: ClipOval(
-                                  child: Image.asset(
-                                    imageAssetPath,
-                                    fit: BoxFit.fitWidth,
-                                    height: screenHeight * 0.1,
-                                    width: screenWidth * 0.163,
+                                  child: Container(
+                                    margin: EdgeInsets.all(6),
+                                    child: ColorFiltered(
+                                      colorFilter: ColorFilter.mode(
+                                        isSelected ? Color(0xFFF8F8F8) : Color(0xFFC2C2C2),
+                                        BlendMode.srcATop,
+                                      ),
+                                      child: Image.asset(
+                                        imageAssetPath,
+                                        fit: BoxFit.cover,
+                                        height: screenHeight * 0.07,
+                                        width: screenWidth * 0.1,
+                                      ),
+                                    ),
                                   ),
                                 ),
                               ),
@@ -146,113 +124,139 @@ class HomeScreen extends StatelessWidget {
                         );
                       }
                     },
-                  );
-                }
-              }),
-            ),
+                  ),
+                );
+              }
+            }),
             SizedBox(height: 20,),
-            Expanded(
-              child: Obx(() {
-                if (homeController.isLoading.value) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (homeController.infoFootballModel.isEmpty) {
-                  return const Center(child: Text('No teams found'));
-                } else {
-                  return ListView.builder(
-                    itemCount: homeController.infoFootballModel.length,
+            Text(
+              'Popular Match',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 15,),
+            Obx(() {
+              if (homeController.isLoading.value) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (homeController.infoEventModel.isEmpty || homeController.infoEventModel.every((event) => event.event == null || event.event!.isEmpty)) {
+                return const Center(child: Text('No events found'));
+              } else {
+                PageController _pageController = PageController(viewportFraction: 1);
+
+                return SizedBox(
+                  height: screenHeight * 0.3,
+                  child: PageView.builder(
+                    controller: _pageController,
+                    itemCount: homeController.infoEventModel.expand((event) => event.event!).where((event) => event.strLeague != '0').length,
                     itemBuilder: (context, index) {
-                      final team = homeController.infoFootballModel[index];
-                      if (team.teams != null && team.teams!.isNotEmpty) {
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: team.teams!.map((team) {
-                            return Card(
-                              color: Colors.white,
-                              surfaceTintColor: Colors.white,
-                              margin: EdgeInsets.symmetric(vertical: 8),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                side: BorderSide(color: Colors.grey.shade800),
+                      final event = homeController.infoEventModel.expand((event) => event.event!).where((event) => event.strLeague != '0').toList()[index];
+
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 5),
+                        child: Card(
+
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Container(
+                            width: 100,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: Color(0xFF330039),
+                              image: DecorationImage(
+                                image: AssetImage('assets/images/premier-bg.png'),
+                                fit: BoxFit.fitWidth,
+                                opacity: 1.5,
                               ),
-                              child: ListTile(
-                                contentPadding: EdgeInsets.all(10),
-                                title: Container(
-                                  margin: EdgeInsets.symmetric(horizontal: 15),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment: CrossAxisAlignment.start,
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    event.strLeague ?? 'No League',
+                                    style: GoogleFonts.lexend(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                  Text(
+                                    event.dateEvent ?? 'No League',
+                                    style: GoogleFonts.lexend(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  SizedBox(height: screenHeight * 0.05),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
                                     children: [
-                                      Row(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                      Column(
                                         children: [
-                                          Image.network(
-                                            team.strBadge ?? '',
-                                            width: screenWidth * 0.145,
-                                            height: screenHeight * 0.08,
-                                            fit: BoxFit.fill,
-                                          ),
-                                          SizedBox(width: 10,),
-                                          Expanded(
-                                            child: Column(
-                                              crossAxisAlignment: CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  team.strTeam ?? '',
-                                                  style: TextStyle(
-                                                    fontWeight: FontWeight.w600,
-                                                  ),
-                                                  overflow: TextOverflow.ellipsis,
-                                                  maxLines: 1,
-                                                ),
-                                                SizedBox(height: 5,),
-                                                Text(
-                                                  team.strTeamAlternate ?? '',
-                                                  style: TextStyle(
-                                                    fontWeight: FontWeight.w500,
-                                                    color: Colors.grey.shade600,
-                                                    fontSize: screenWidth * 0.026,
-                                                  ),
-                                                  overflow: TextOverflow.ellipsis,
-                                                  maxLines: 3,
-                                                ),
-                                              ],
+                                          Container(
+                                            width: 80,
+                                            height: 80,
+                                            child: Image.network(
+                                              event.strHomeTeamBadge ?? 'No Image',
+                                              errorBuilder: (context, error, stackTrace) {
+                                                return Image.network(homeController.infoFootballModel[0].teams?[0].strBadge ?? '');
+                                              },
                                             ),
                                           ),
+                                          SizedBox(height: 5,),
+                                          Text(event.strHomeTeam ?? '', style: TextStyle(color: Colors.white),)
                                         ],
                                       ),
-                                      SizedBox(height: 20,),
-                                      Container(
-                                        child: Row(
-                                          children: [
-                                            Categorycard(
-                                              text: team.strLeague ?? '',
-                                              fontSize: screenWidth * 0.022,
+                                      Row(
+                                        children: [
+                                          Text(event.intHomeScore ?? 'N/A', style: TextStyle(color: Colors.white, fontSize: screenWidth * 0.07, fontWeight: FontWeight.w600),),
+                                          SizedBox(width: 10,),
+                                          Text('-', style: TextStyle(color: Colors.white, fontSize: screenWidth * 0.07, fontWeight: FontWeight.w600),),
+                                          SizedBox(width: 10,),
+                                          Text(event.intAwayScore ?? 'N/A', style: TextStyle(color: Colors.white, fontSize: screenWidth * 0.07, fontWeight: FontWeight.w600  ),),
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          Container(
+                                            width: 80,
+                                            height: 80,
+                                            child: Image.network(
+                                              event.strAwayTeamBadge ?? 'No Image',
+                                              errorBuilder: (context, error, stackTrace) {
+                                                return Image.network(
+                                                  (homeController.infoFootballModel.isNotEmpty &&
+                                                      homeController.infoFootballModel[0].teams != null &&
+                                                      homeController.infoFootballModel[0].teams!.length > 5)
+                                                      ? homeController.infoFootballModel[0].teams![5].strBadge ?? 'assets/images/default-badge.png'
+                                                      : 'assets/images/default-badge.png', // Fallback to a default image if not found
+                                                );
+                                              },
                                             ),
-                                            SizedBox(width: 10,),
-                                            Categorycard(
-                                              text: team.strStadium ?? '',
-                                              fontSize: screenWidth * 0.022,
-                                            ),
-                                          ],
-                                        ),
-                                      )
+                                          ),
+                                          SizedBox(height: 5,),
+                                          Text(event.strAwayTeam ?? '', style: TextStyle(color: Colors.white),)
+                                        ],
+                                      ),
                                     ],
                                   ),
-                                ),
+                                ],
                               ),
-                            );
-                          }).toList(),
-                        );
-                      } else {
-                        return const ListTile(
-                          title: Text('No team information available'),
-                        );
-                      }
+                            ),
+                          ),
+                        ),
+                      );
                     },
-                  );
-                }
-              }),
-            ),
+                  ),
+                );
+              }
+            }),
           ],
         ),
       ),
